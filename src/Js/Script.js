222a58e5f8d26be4070dd2d25b1625c2d040d272
Edwin <Edwin4lexander@gmail.com>
2026-01-31 11:52:48 +0700

    Fixed images to now be visible

--- src/Js/Script.js
+++ src/Js/Script.js
@@ -5,30 +5,30 @@
 
 const foods = [
     {
-        flag:"./Asset/Bulgaria",
-        image:"./Asset/Goulash",
-        Country:"Hungary",
+        flag:"https://cdn.britannica.com/55/1455-050-CCDFFCF0/Flag-Hungary.jpg?w=400&h=300&c=crop",
+        image:"https://www.simplyrecipes.com/thmb/1P8E9m3nTYPkvNu4ig3SqlSYcZ0=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/simply-recipes-hungarian-goulash-lead-1-9b243ad244134164aed863c874215ffa.jpg",
+        country:"Hungary",
         title: "Goulash",
         description:"A soup made with a variety of meat and vegetables, seasoned with paprika and other spices. "
     }, 
     {
-        flag:"./Asset/Japan",
-        image:"./Asset/Ochazuke",
-        Country:"Japan",
+        flag:"https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/330px-Flag_of_Japan.svg.png",
+        image:"https://thejapanstore.us/wp-content/uploads/2021/11/shutterstock_1184082958-scaled.jpg",
+        country:"Japan",
         title:"Ochazuke",
         description:"A simple rice bowl that adds green tea over cooked rice with toppings like salmon, seaweed, etc."
     },
     {
-        flag:"./Asset/China",
-        image:"./Asset/Mapo_Tofu",
-        Country:"China",
+        flag:"https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/1280px-Flag_of_the_People%27s_Republic_of_China.svg.png",
+        image:"https://redhousespice.com/wp-content/uploads/2022/05/mapo-tofu-in-a-plate-500x375.jpg",
+        country:"China",
         title:"Mapo Tofu",
         description:"A popular dish consisting of tofu and spicy beans. Originating from Sichuan representing the Mala style."
     },
     {
-        flag:"./Asset/Vietnam",
-        image:"./Asset/Pho",
-        Country:"Vietnam",
+        flag:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1280px-Flag_of_Vietnam.svg.png",
+        image:"https://www.recipetineats.com/tachyon/2019/04/Beef-Pho_6.jpg?resize=500%2C500",
+        country:"Vietnam",
         title:"Pho",
         description:"A soup that has noodles that are made with rice flour. Made all over in street stalls and households."
     }
